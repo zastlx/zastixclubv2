99feb48820552574cbfb26afabe57c413f8d9c4f
zastlx <zastix@zastix.club>
2025-02-17 20:43:19 -0600

    font awsome fix and more friends

--- frontend/src/utils/friendsStore.tsx
+++ frontend/src/utils/friendsStore.tsx
@@ -20,6 +20,9 @@ export function FriendStoreProvider({ children }: { children: React.ReactNode })
             { id: "1021090674289942600", link: "https://crit.rip" },
             { id: "190733468550823945", link: "https://github.com/ItsSyfe" },
             { id: "709534876361162765", link: "https://www.youtube.com/@Pig_55" },
+            { id: "1186832040197361746", link: "https://www.onedumbduck.com/" },
+            { id: "812938084375199804", link: "https://discord.gg/yyu2QzhbDA" },
+            { id: "1197702222893547590", link: "https://doyle31.com/" }
         ];
 
         const fetchFriends = async () => {
