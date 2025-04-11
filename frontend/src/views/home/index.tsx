c70ec248456b4ad978f4982b08cc58681aded3d1
zastlx <zastix@zastix.club>
2025-04-10 22:51:04 -0500

    put java and bongus always next to eachother

--- frontend/src/views/home/index.tsx
+++ frontend/src/views/home/index.tsx
@@ -24,7 +24,16 @@ export default function Home() {
             <div className={styles.friendsWrapper}>
                 <h1>Check out these cute people too!</h1>
                 <div className={styles.friends}>
-                    {friendStore.friends.sort(() => Math.random() - 0.5).map((friend, index) => (
+                    {(() => {
+                        // put java and bongus next to eachother
+                        const friends = friendStore.friends;
+                        const group = ["570358869235073042", "812938084375199804"];
+                        const pair = friends.filter(f => group.includes(f.id));
+                        const rest = friends.filter(f => !group.includes(f.id)).sort(() => Math.random() - 0.5);
+
+                        rest.splice(Math.floor(Math.random() * (rest.length + 1)), 0, ...pair);
+                        return rest;
+                    })().map((friend, index) => (
                         <Friend key={index} {...friend} />
                     ))}
                 </div>
