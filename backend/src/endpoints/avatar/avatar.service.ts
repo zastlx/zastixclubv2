df9aad60e644ddf97159c476719b2b2a2b85e3c5
zastlx <zastix@zastix.club>
2025-05-31 12:26:24 -0500

    a

--- backend/src/endpoints/avatar/avatar.service.ts
+++ backend/src/endpoints/avatar/avatar.service.ts
@@ -32,7 +32,7 @@ export class AvatarService {
 	async getAvatar(userid: string): Promise<string> {
 		try {
 			const user = await this.fetchUser(userid);
-			return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=64`;
+			return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size4096`;
 		} catch (e) {
 			throw e;
 		}
